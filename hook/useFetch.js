9260865a1bc9e3c557869a43a8b63c5c37a134df
Simiyu-coder <simiyukelvin93@gmail.com>
2023-05-26 10:46:31 +0300

    fix error message

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,51 +1,47 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      'X-RapidAPI-Key':'802e877424msha0e7484359feca5p10b238jsn09494d86248d' ,
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+    },
+    params: { ...query },
+  };
 
-const useFetch = ( endpoint, query) => {
-    const [ data, setData] = useState([]);
-    const [ isLoading, setIsLoading] = useState(false);
-    const [ error, setError] = useState(null);
+  const fetchData = async () => {
+    setIsLoading(true);
 
-
-
-const options = {
-  method: 'GET',
-  url: 'https://jsearch.p.rapidapi.com/${endpoint}',
-  headers: {
-    'X-RapidAPI-Key':'802e877424msha0e7484359feca5p10b238jsn09494d86248d' ,
-    'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-  },
-  params: { ...query}, 
-};
-
-
-const fetchData = async () => {
-  setIsLoading(true);
     try {
-	    const response = await axios.request (options);
+      const response = await axios.request(options);
+
       setData(response.data.data);
       setIsLoading(false);
     } catch (error) {
-      setError(error)
-      alert('There is an Error!')
-    } finally{
-        setIsLoading(false);
+      setError(error);
+      console.log(error)
+    } finally {
+      setIsLoading(false);
     }
- }
+  };
 
- useEffect( () => {
+  useEffect(() => {
     fetchData();
- }, []);
+  }, []);
 
-
- const refetch = () => {
+  const refetch = () => {
     setIsLoading(true);
     fetchData();
- }
+  };
 
- return { data, isLoading,error, refetch}
-}
+  return { data, isLoading, error, refetch };
+};
 
 export default useFetch;
